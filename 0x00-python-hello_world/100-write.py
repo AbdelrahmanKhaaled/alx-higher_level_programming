0e4e60861c39c8bee8373c1404e9433e322f2d40
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-02 21:30:58 +0000

    Python - Hello, World

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-f.open(sys.stderr,"w")
-f.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-f.close()
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
