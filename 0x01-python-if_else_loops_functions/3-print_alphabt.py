0dde8ac4cf595df077c2aca012f6f3ce7f3cee67
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-03 01:35:55 +0000

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 num = 97
 while (num <= 122):
-    if (num == 101 or num == 113)
+    if (num == 101 or num == 113):
         num = num + 1
         continue
     print("{:c}".format(num), end="")
