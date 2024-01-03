4b4957d44f3ddbb5e247f17f94315fc928b50707
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-03 00:06:43 +0000

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for num in range(100):
     if (num < 10):
         print(f"0{num}, ", end="")
     else:
-        print("{}".format(f"{num}, " if num <= 99 else "99\n"), end="")
+        print("{}".format(f"{num}, " if num < 99 else "99\n"), end="")
