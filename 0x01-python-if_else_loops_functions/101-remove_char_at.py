affbd50c29848a092763a642f8f11b4c8ccd48be
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-03 01:12:05 +0000

    Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    name[len(str)]
+    count = 0
+    for num in range(len(str)):
+        if (num != n):
+            name[count] = str[num]
+            count = count + 1
