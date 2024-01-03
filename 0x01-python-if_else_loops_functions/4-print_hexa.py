a1e83adba59b94ecb94a83655f556ea5c7906f01
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-03 02:34:30 +0000

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 for num in range(99):
-    if (num % 16 <= 9 and num / 16 > 0):
-        print(f"0x{num}")
-    elif (num % 16 > 9 and num % 16 < 16 and num / 16 > 0):
-        print(f"0x{:c}".format(num + 87))
-    if (num % 16 <= 9):
-        print(f"0x{num / 16}{num % 16}")
-    elif (num % 16 > 9 and num % 16 < 16):
-        print("0x{:d}{:c}".format(num / 16, (num % 16) + 87)))
+    print(f"{:d} = 0x{:x}".format(num, num))
