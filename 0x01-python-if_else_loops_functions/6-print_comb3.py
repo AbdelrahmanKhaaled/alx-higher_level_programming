7a5a30d75a327e33628549c4d270bfb0b3e4b439
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-03 00:15:03 +0000

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num in range(9):
     for num2 in range(9):
-        if (num == num2 and num > num2):
+        if (num == num2 or num > num2):
             continue
         else:
             print("{:d}{:d}, ".format(num, num2), end="")
