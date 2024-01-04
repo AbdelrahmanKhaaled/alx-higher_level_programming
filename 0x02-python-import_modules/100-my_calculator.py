d8d0a7b4eb8472aff114aeaedfd995d85b36a70a
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-04 18:38:06 +0000

    Python - import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,16 +9,16 @@ if __name__ == "__main__":
         exit(1)
     else:
         if args[1] == "+":
-            print("{} + {} = {}".format(args[0], args[2], add(args[0], args[2])))
+            print("{} + {} = {}".format(int(args[0]), int(args[2]), add(int(args[0]), int(args[2]))))
             exit(0)
         elif args[1] == "-":
-            print("{} - {} = {}".format(args[0], args[2], sub(args[0], args[2])))
+            print("{} - {} = {}".format(int(args[0]), int(args[2]), sub(int(args[0]), int(args[2]))))
             exit(0)
         elif args[1] == "*":
-            print("{} * {} = {}".format(args[0], args[2], mul(args[0], args[2])))
+            print("{} * {} = {}".format(int(args[0]), int(args[2]), mul(int(args[0]), int(args[2]))))
             exit(0)
         elif args[1] == "/":
-            print("{} / {} = {}".format(args[0], args[2], div(args[0], args[2])))
+            print("{} / {} = {}".format(int(args[0]), int(args[2]), div(int(args[0]), int(args[2]))))
             exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
