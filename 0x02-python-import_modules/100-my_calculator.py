dad26886f020272f8e867d44d7fd294e7bbba13c
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-04 19:02:11 +0000

    Python - import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,19 +7,14 @@ if __name__ == "__main__":
     if len(args) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    if args[1] == "+":
+        print("{} + {} = {}".format(int(args[0]), int(args[2]), add(int(args[0]), int(args[2]))))
+    elif args[1] == "-":
+        print("{} - {} = {}".format(int(args[0]), int(args[2]), sub(int(args[0]), int(args[2]))))i
+    elif args[1] == "*":
+        print("{} * {} = {}".format(int(args[0]), int(args[2]), mul(int(args[0]), int(args[2]))))
+    elif args[1] == "/":
+        print("{} / {} = {}".format(int(args[0]), int(args[2]), div(int(args[0]), int(args[2]))))
     else:
-        if args[1] == "+":
-            print("{} + {} = {}".format(int(args[0]), int(args[2]), add(int(args[0]), int(args[2]))))
-            exit(0)
-        elif args[1] == "-":
-            print("{} - {} = {}".format(int(args[0]), int(args[2]), sub(int(args[0]), int(args[2]))))
-            exit(0)
-        elif args[1] == "*":
-            print("{} * {} = {}".format(int(args[0]), int(args[2]), mul(int(args[0]), int(args[2]))))
-            exit(0)
-        elif args[1] == "/":
-            print("{} / {} = {}".format(int(args[0]), int(args[2]), div(int(args[0]), int(args[2]))))
-            exit(0)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
