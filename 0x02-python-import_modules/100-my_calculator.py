3edbe7e8aaedce95af2d119b68d5ae8a4ffe2884
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-04 18:27:43 +0000

    Python - import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
 
     args = sys.argv
     if len(args) != 4:
