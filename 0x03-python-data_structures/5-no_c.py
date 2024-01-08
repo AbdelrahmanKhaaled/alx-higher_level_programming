ef982235f7cd8974642a080845c045da76fe9c7f
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-08 16:00:22 +0000

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,10 +4,9 @@ def no_c(my_string):
     str2 = ""
     for c in my_string:
         if c == "c" or c == "C":
-            if (len(my_string[:end]) > 0):
-                str2 += my_string[:end]
-                my_string = my_string[end + 1:]
-                end = -1
+            str2 += my_string[:end]
+            my_string = my_string[end + 1:]
+            end = -1
         end += 1
     str2 += my_string
     return str2
