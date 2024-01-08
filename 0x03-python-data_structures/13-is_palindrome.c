45afb2d45a6c218007a9fa2379c4165eeb80c46b
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-08 15:17:36 +0000

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,6 +4,7 @@
  * is_palindrome - checks if a singly linked list is a palindrome.
  *
  * @head: checks input of function
+ * * Return: 0 if it is not a palindrome, 1 if it is a palindrome
 */
 
 int is_palindrome(listint_t **head)
