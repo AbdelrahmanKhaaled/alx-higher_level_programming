c77dc1c8583feeeb794b37a6f06026001ab9d4ef
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-10 21:23:37 +0000

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if (a_dictionary == None) or (len(a_dictionary) == 0):
+    if a_dictionary is None:
         return None
-    else:
-        Max = list(a_dictionary.values())[0]
-        target_key = list(a_dictionary.keys())[0]
-        for key, value in a_dictionary.items():
-            if Max < value:
-                target_key = key
-                Max = value
-        return target_key
+    Max = 0
+    target_key = None
+    for key, value in a_dictionary.items():
+        if Max < value:
+            target_key = key
+            Max = value
+    return target_key
