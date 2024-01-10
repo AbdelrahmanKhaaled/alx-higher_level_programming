98ddb2d1dbd29c7c542d821ba75b37f860bf0619
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-10 21:21:37 +0000

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     else:
         Max = list(a_dictionary.values())[0]
         target_key = list(a_dictionary.keys())[0]
-        for key, value in list(a_dictionary.items()):
+        for key, value in a_dictionary.items():
             if Max < value:
                 target_key = key
                 Max = value
