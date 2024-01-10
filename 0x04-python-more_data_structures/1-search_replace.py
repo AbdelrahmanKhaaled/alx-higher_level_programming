ef468c85aadfd7a1fa9dbe7ca093ea4e40679fa8
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-01-10 20:31:32 +0000

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for num in my_list:
-        if num == 2:
-            new_list.append(89)
+        if num == search:
+            new_list.append(replace)
         else:
             new_list.append(num)
     return new_list
