8e413c4733a7f2d72135517e93b00ca33a144395
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-03-05 16:51:34 +0000

    Python - Test-driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """ function that adds 2 integers.
+    Args:
+        a: first number.
+        b: second number.
+    
+    Rasies:
+        TypeError: if a and b are not integer, float.
+    
+    Returns:
+        the summation of two integers.
+    """
+
+    if type(a) not in(float, int):
+        raise TypeError("a must be an integer")
+    if type(b) not in(float, int):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
