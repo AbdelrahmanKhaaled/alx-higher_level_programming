a4b7c4925e850b4720b6ac7e11ea9b412c781b39
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-03-06 19:11:24 +0000

    Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,14 +3,15 @@
 from models.base import Base
 
 class Rectangle(Base):
-    '''A representation of the rectangle of our OOP hierarchy.'''
+    '''A rectangleclass.'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(self, id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        '''Constructor.'''
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
