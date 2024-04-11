a76bb63b2404bddf90d12628a713312b7ff00159
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-11 15:18:53 +0000

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
 exports.dict = require('./100-data').dict;
 
 let newdic = {};
 for (let [key, value] of Object.entries(dict)) {
-	list = []
+	const list = [];
 	for (let [key2, value2] of Object.entries(dict)) {
 		if (value === value2)
 			list.push(key2);
