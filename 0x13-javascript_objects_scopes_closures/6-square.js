d8dc6412b860b2d769a5d645435a5e4e429341c8
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-11 18:56:18 +0000

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,24 +3,22 @@
 const SquareOld = require('./5-square');
 
 class Square extends SquareOld {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
 
-	constructor (size) {
-		super(size);
-		this.size = size;
-	}
-
-	charPrint (c) {
-		for (let i = 0; i < this.size; i++) {
-			for (let j = 0; j < this.size; j++) {
-				if (c === undefined)
-					c = "X";
-				if (j != this.size - 1)
-					process.stdout.write(c);
-				else
-					console.log(c);
-			}
-		}
-	}
+  charPrint (c) {
+    for (let i = 0; i < this.size; i++) {
+      let str = '';
+      for (let j = 0; j < this.size; j++) {
+        if (c === undefined)
+          c = "X";
+	str += 'X';
+      }
+      console.log(str);
+    }
+  }
 }
 
 module.exports = Square;
