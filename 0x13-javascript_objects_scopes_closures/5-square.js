306bc8a11620c0b608eca7181a7a0ae883284c2a
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-11 15:51:32 +0000

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,9 +2,11 @@
 
 const Rectangle = require('./4-rectangle');
 
-module.exports = class Square extends Rectangle {
+class Square extends Rectangle {
 
 	constructor(size) {
 		super(size, size);
 	}
 }
+
+module.exports = Square;
