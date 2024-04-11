9aeaec6f90986e9bcebda85a28946c7ac6406251
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-11 15:38:06 +0000

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,8 +2,8 @@
 
 module.exports = class Rectangle {
 
-	constructor (w,h) {
-		if (w > 0 && h > 0) {
+	constructor (w, h) {
+		if ((w > 0) && (h > 0)) {
 			this.width = w;
 			this.height = h;
 		}
@@ -11,12 +11,11 @@ module.exports = class Rectangle {
 
 	print () {
 		for (let i = 0; i < this.height; i++) {
+			let str = '';
 			for (let j = 0; j < this.width; j++) {
-				if (j != this.width - 1)
-					process.stdout.write("X");
-				else
-					console.log("X");
+				str += 'X';
 			}
+			console.log(str);
 		}
 	}
 
