6c6e75d91bb55db9447c24d4cd84797c1355a1a4
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-14 19:46:45 +0000

    Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' Module that lists all states from the database '''
+import sys
+import MySQLdb
+
+def cities_by_state(username, password, database):
+
+    db = MySQLdb.connect(host='localhost',  port=3306, user=username, passwd=password, db=database)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * from cities, states where state_id = id order by id;")
+    state = cursor.fetchall()
+    for s in state:
+        print(s)
+    db.close()
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    cities_by_state(username, password, database)
