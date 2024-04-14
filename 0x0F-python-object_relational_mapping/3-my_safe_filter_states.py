f99b41d4acfdd9851f19ed838c574495e6698a52
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-14 19:07:45 +0000

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,16 +6,16 @@ import MySQLdb
 
 def my_filter_states(username, password, database, searchName):
 
-    db = MySQLdb.connect(host='localhost',  port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host=localhost,  port=3306, user=username, passwd=password, db=database)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * from states where binary name = '{}' order by id;".format(searchName))
+    cursor.execute("SELECT * from states where binary name = '{}'' order by id".format(searchName))
     state = cursor.fetchall()
     for s in state:
         print(s)
     db.close()
 
-if __name__ == '__main__':
+if __name__ == __main__:
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
