b4cf985f2946c4dbd8bd06b403f41341ee9ff7c4
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-14 15:03:12 +0000

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ def my_filter_states(username, password, database, searchName):
     db = MySQLdb.connect(host='localhost',  port=3306, user=username, passwd=password, db=database)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * from states where name like {} order by id;".format(searchName))
+    cursor.execute("SELECT * from states where binary name = {} order by id;".format(searchName))
     state = cursor.fetchall()
     for s in state:
         print(s)
