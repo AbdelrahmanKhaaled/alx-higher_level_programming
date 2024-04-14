2926c2ba3b11ff27fc732b02dfd26ba4eff85de3
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-14 14:13:03 +0000

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,7 +15,7 @@ def list_AllStates(username, password, database):
         print(s)
     db.close()
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
