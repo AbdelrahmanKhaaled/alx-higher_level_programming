7848301265f8d746ffb6a33173ffb5e92ecdb131
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-14 19:20:46 +0000

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 ''' Module that takes in an argument and displays all values in
-the states table of hbtn_0e_0_usa where name matches the argument. '''
+the states table of hbtn_0e_0_usa where name matches the argument.'''
 import sys
 import MySQLdb
 
-def my_filter_states(username, password, database, searchName):
+def my_safe_filter_states(username, password, database, searchName):
 
     db = MySQLdb.connect(host=localhost,  port=3306, user=username, passwd=password, db=database)
 
@@ -21,4 +21,4 @@ if __name__ == __main__:
     database = sys.argv[3]
     searchName = sys.argv[4]
 
-    my_filter_states(username, password, database, searchName)
+    my_safe_filter_states(username, password, database, searchName)
