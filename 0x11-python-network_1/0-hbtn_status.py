b3aaf3a3bed23bb235b81f696e2d22376562001b
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-26 16:17:28 +0000

    Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,10 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = f'https://api.github.com/users/{username}'
-    header = {'Authorization': f'Bearer {tocken}'}
-    response = urllib.request.Request(url, headers=header)
-    with urllib.request.urlopen(response) as req:
-        r = req.read()
-        head = req.headers
-    print(r)
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body_response = response.read()
+        print("Body response:")
+        print("\t- type:", type(body_response))
+        print("\t- content:", body_response.decode())
