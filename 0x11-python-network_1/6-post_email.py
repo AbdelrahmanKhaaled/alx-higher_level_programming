9cde70a31d7ffd36b1df6c735d74302a91dabddd
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-26 20:09:57 +0000

    Python - Network #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,7 +4,12 @@
 import requests
 import sys
 
-def send_post_request(url, email):
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <URL> <email>")
+        sys.exit(1)
+    url = sys.argv[1]
+    email = sys.argv[2]
     try:
         payload = {'email': email}
         response = requests.post(url, data=payload)
@@ -12,10 +17,3 @@ def send_post_request(url, email):
         print(response.text)
     except requests.RequestException as e:
         print("Error sending POST request:", e)
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <URL> <email>")
-        sys.exit(1)
-    url = sys.argv[1]
-    email = sys.argv[2]
-    send_post_request(url, email)
