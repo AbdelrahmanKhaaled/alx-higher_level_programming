960cf7e150a4938ea8c2fc4ff2d3116cfac28a4b
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-26 17:21:46 +0000

    Python - Network #1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,8 +12,7 @@ url = sys.argv[1]
 email = sys.argv[2]
 
 data = urllib.parse.urlencode({'email': email}).encode()
-req = urllib.request.Request(url, data=data, method='POST')
 
-with urllib.request.urlopen(req) as response:
+with urllib.request.urlopen(url, data=data) as response:
     body_response = response.read().decode('utf-8')
     print("Your email is:", email)
