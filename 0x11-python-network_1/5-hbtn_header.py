c7d57d2fdf10f838c8ed984abc103af694c5ea2d
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-26 17:31:08 +0000

    Python - Network #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+import sys
+
+def get_request_id(url):
+    try:
+        response = requests.get(url)
+        request_id = response.headers.get('X-Request-Id')
+        if request_id:
+            print("X-Request-Id:", request_id)
+        else:
+            print("X-Request-Id not found in response headers.")
+    except requests.RequestException as e:
+        print("Error fetching URL:", e)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+    url = sys.argv[1]
+    get_request_id(url)
