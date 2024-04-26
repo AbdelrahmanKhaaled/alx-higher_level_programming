c169cc132c07d1618112b8c08997007a4335d2c1
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-26 16:23:17 +0000

    Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         body_response = response.read()
         print("Body response:")
-        print("     - type:", type(body_response))
-        print("     - content:", body_response.decode())
-        print("     - utf8 content:", body_response.decode())
+        print("    - type:", type(body_response))
+        print("    - content:", body_response.decode())
+        print("    - utf8 content:", body_response.decode())
