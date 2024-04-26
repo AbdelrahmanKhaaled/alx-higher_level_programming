fc6cc8a233349ec8fa4a024000423a77fa311f29
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-26 16:01:53 +0000

    Python - Network #1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import urllib.request
+url = f'https://api.github.com/users/{username}'
+header = {'Authorization': f'Bearer {tocken}'}
+response = urllib.request.Request(url, headers=header)
+with urllib.request.urlopen(response) as req:
+    r = req.read()
+    head = req.headers
+print(r)
