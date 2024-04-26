b4a3188595ef74698c462d42f0215aea313ed105
Abdelrahman_Khaled <abdoeka534@gmail.com>
2024-04-26 14:59:24 +0000

    Python - Network #0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #takes in a URL, sends a GET request to the URL, and displays the body of the response
-url=$1
-response=$(curl -s -w "%{http_code}" "$url")
-[ "${response: -3}" -eq 200 ] && curl -s "$url"
+curl -s -w "\n%{http_code}\n" "$1" | awk '/^$/ {body=1; next} body && !/^2/'
